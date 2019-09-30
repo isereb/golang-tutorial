2d43e9e242b10df5b2946df7c216d1c84fd5a1d8
isereb <ruxxay.coder@gmail.com>
2019-09-30 22:32:26 +0000

    Ninja 8: Exercise done is one

---
+++ ninja/level8/ex1/ex1.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type user struct {
+	First string
+	Age   int
+}
+
+func main() {
+	u1 := user{
+		First: "James",
+		Age:   32,
+	}
+
+	u2 := user{
+		First: "Moneypenny",
+		Age:   27,
+	}
+
+	u3 := user{
+		First: "M",
+		Age:   54,
+	}
+
+	users := []user{u1, u2, u3}
+
+	fmt.Println("String():\t", users)
+
+	bytes, err := json.Marshal(users)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Bytes:\t\t", bytes)
+	fmt.Println("JSON:\t\t", string(bytes))
+
+	// your code goes here
+}
