4d85f83255f2ca62918c327b67cfcd0e851c12f6
isereb <ruxxay.coder@gmail.com>
2019-10-01 00:12:16 +0000

    Added wait group to add chaos

--- ninja/level9/prep2/raceConditionTest.go
+++ ninja/level9/prep2/raceConditionTest.go
@@ -1,19 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
 
 	amt := 100
 	counter := 0
 
+	var wg sync.WaitGroup
+	wg.Add(amt)
 	for j := 0; j < amt; j++ {
 		fmt.Println("START: Iteration: ", j, "Value:", counter)
 		go func() {
 			v := counter
 			v++
 			counter = v
+			wg.Done()
 		}()
 		fmt.Println("END: Iteration: ", j, "Value:", counter)
 	}
+	wg.Wait()
 }
