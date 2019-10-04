1124d72bced5e73e69a830e73645ba92fdf239bc
isereb <ruxxay.coder@gmail.com>
2019-10-04 22:44:53 +0000

    Ninja 10: Exercise done is five

---
+++ ninja/level10/ex5/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	c := make(chan int)
+
+	go func() {
+		c <- 1
+		c <- 2
+	}()
+
+	v, ok := <-c
+	fmt.Println(v, ok)
+
+	close(c)
+
+	v, ok = <-c
+	fmt.Println(v, ok)
+}
