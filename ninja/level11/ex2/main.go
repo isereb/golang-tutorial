2623cf723569e2de1611c9d0d72fe6daf64974b6
isereb <ruxxay.coder@gmail.com>
2019-10-05 00:28:19 +0000

    Ninja 11: Exercise done is two

---
+++ ninja/level11/ex2/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type person struct {
+	First   string
+	Last    string
+	Sayings []string
+}
+
+func main() {
+	p1 := person{
+		First:   "James",
+		Last:    "Bond",
+		Sayings: []string{"Shaken, not stirred", "Any last wishes?", "Never say never"},
+	}
+
+	bs, err := toJSON(p1)
+	if err != nil {
+		fmt.Println("ERROR ERROR 42")
+	}
+
+	fmt.Println(string(bs))
+
+}
+
+// toJSON needs to return an error also
+func toJSON(a interface{}) ([]byte, error) {
+	bs, err := json.Marshal(a)
+	if err != nil {
+		return []byte{}, fmt.Errorf("There was an error when Marshalling JSON")
+	}
+	return bs, nil
+}
