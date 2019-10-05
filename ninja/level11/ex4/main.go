91be5e064fce51a1393756d237c2ea02f909c43c
isereb <ruxxay.coder@gmail.com>
2019-10-05 00:47:46 +0000

    Ninja 11: Exercise done is four

---
+++ ninja/level11/ex4/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+)
+
+type sqrtError struct {
+	lat  string
+	long string
+	err  error
+}
+
+func (se sqrtError) Error() string {
+	return fmt.Sprintf("math error: %v %v %v", se.lat, se.long, se.err)
+}
+
+func main() {
+	_, err := sqrt(-10.23)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func sqrt(f float64) (float64, error) {
+	if f < 0 {
+		err := sqrtError{
+			lat:  "50.23274 N",
+			long: "9.12572 W",
+			err:  errors.New("More coffee needed")}
+		return 0, err
+	}
+	return 42, nil
+}
