6f088c854c1dabe392019d375cffeea9ab64d8f9
isereb <ruxxay.coder@gmail.com>
2019-10-05 00:20:00 +0000

    Ninja 11: Exercise done is one

---
+++ ninja/level11/ex1/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type person struct {
+	First   string
+	Last    string
+	Sayings []string
+}
+
+func main() {
+	p1 := person{
+		First:   "James",
+		Last:    "Bond",
+		Sayings: []string{"Shaken, not stirred", "Any last wishes?", "Never say never"},
+	}
+
+	bs, err := json.Marshal(p1)
+	if err != nil {
+		log.Fatal("Ooops. Something's wrong with JSON... Error:", err)
+	}
+	fmt.Println(string(bs))
+}
